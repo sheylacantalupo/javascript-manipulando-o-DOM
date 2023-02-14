f46658875a8fa8e1198f54241b1907b6922409ce
Sheyla Cantalupo <sheylacantalupo.sc@gmail.com>
2023-02-14 08:59:03 -0300

    métodos js

---
+++ robotrom_2000/main.js
@@ -0,0 +1,11 @@
+
+// elementos podem ficar dentro de uma variável
+const robotron = document.querySelector('#robotron');
+
+robotron.addEventListener("click", dizOi );
+
+function dizOi () {
+
+    console.log("oi");
+
+}
