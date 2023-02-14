66ba9b17b224bb58a9272aff728243507e30ce52
Sheyla Cantalupo <sheylacantalupo.sc@gmail.com>
2023-02-14 12:08:41 -0300

    desagio 02, manipulando elementos

---
+++ desafio_02/main.js
@@ -0,0 +1,11 @@
+
+const botao = document.querySelector('#calcular');
+
+const resultado = document.querySelector('.resultado');
+
+botao.addEventListener("click", () =>{
+
+    resultado.innerHTML = "Fui clicado";
+
+})
+
